b63e92600a1171255abdd1c55645d007731699c9
Lucas Dietrich <lucas.dietrich.git@proton.me>
2026-01-14 21:04:01 +0100

    Add a dependency only buildable with rustc >= 1.85.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!")
+    let mut bufs = vec![vec![]; 5];
+
+    rmp::encode::write_pfix(&mut bufs[0], 42).unwrap();
+    rmp::encode::write_u8(&mut bufs[1], 42).unwrap();
+    rmp::encode::write_u16(&mut bufs[2], 42).unwrap();
+    rmp::encode::write_u32(&mut bufs[3], 42).unwrap();
+    rmp::encode::write_u64(&mut bufs[4], 42).unwrap();
+
+    println!("{:?}", bufs);
+
+    assert_eq!([0x2a], bufs[0][..]);
+    assert_eq!([0xcc, 0x2a], bufs[1][..]);
+    assert_eq!([0xcd, 0x00, 0x2a], bufs[2][..]);
+    assert_eq!([0xce, 0x00, 0x00, 0x00, 0x2a], bufs[3][..]);
+    assert_eq!([0xcf, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2a], bufs[4][..]);
 }
